a712466ccd2ac9aff27976f2dde5fdae19b7d38d
Qma <mk348437@gmail.com>
2023-07-07 12:51:43 +0200

    created editor bootstrapper;

---
+++ Assets/Project/Architecture/Boot/EditorBootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Project.Architecture
+{
+    public class EditorBootstrapper : MonoBehaviour
+    {
+        private void Start()
+        {
+            #if !UNITY_EDITOR
+            Destroy(this);
+            return;
+            #endif
+            
+            if (FindObjectOfType<Bootstrapper>())
+                Destroy(this);
+            else
+                SceneManager.LoadScene(0);
+        }
+    }
+}
