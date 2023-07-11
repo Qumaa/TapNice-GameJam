9a3e0398d4266bd534ce3509518894dddb489ccc
Qma <mk348437@gmail.com>
2023-07-11 11:49:17 +0200

    implemented functional main menu;
    changed prefabs, scripts etc.;

---
+++ Assets/Project/UI/Main menu/Functional/FunctionalMainMenu.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Project.Game;
+using UnityEngine;
+
+namespace Project.UI
+{
+    public class FunctionalMainMenu : MonoBehaviour, IMainMenu
+    {
+        [SerializeField] private FunctionalMainMenuButton _buttonPrefab;
+        [SerializeField] private RectTransform _buttonsParent;
+        
+        private FunctionalMainMenuButton[] _buttons;
+        
+        public event Action<int> OnLevelPlayPressed;
+
+        public void SetLevels(ILevelDescriptor[] levels)
+        {
+            _buttons = new FunctionalMainMenuButton[levels.Length];
+
+            for (var i = 0; i < levels.Length; i++)
+                CreateLevelButton(levels[i], i);
+        }
+
+        private void OnDestroy()
+        {
+#if UNITY_EDITOR
+            if (_buttons == null)
+                return;
+#endif
+            
+            foreach (var button in _buttons)
+                button.OnPressed -= EmitEvent;
+        }
+
+        private void CreateLevelButton(ILevelDescriptor level, int index)
+        {
+            var button = CreateButtonObject();
+            button.SetLevel(level.LevelName, index);
+            button.OnPressed += EmitEvent;
+
+            _buttons[index] = button;
+        }
+
+        private FunctionalMainMenuButton CreateButtonObject() =>
+            Instantiate(_buttonPrefab, _buttonsParent, false).GetComponent<FunctionalMainMenuButton>();
+
+        private void EmitEvent(int arg) =>
+            OnLevelPlayPressed?.Invoke(arg);
+    }
+}
---
+++ Assets/Project/UI/Main menu/Functional/FunctionalMainMenuButton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Project.UI
+{
+    public class FunctionalMainMenuButton : MonoBehaviour
+    {
+        [SerializeField] private Button _button;
+        [SerializeField] private TextMeshProUGUI _levelNameLabel;
+        private int _levelIndex;
+        public event Action<int> OnPressed;
+
+        private void OnEnable()
+        {
+            _button.onClick.AddListener(EmitEvent);
+        }
+
+        private void OnDisable()
+        {
+            _button.onClick.RemoveListener(EmitEvent);
+        }
+
+        public void SetLevel(string levelName, int index)
+        {
+            _levelNameLabel.text = GetLevelName(index, levelName);
+            _levelIndex = index;
+        }
+
+        private static string GetLevelName(int index, string name) =>
+            $"{index + 1}. {name}";
+
+        private void EmitEvent() =>
+            OnPressed?.Invoke(_levelIndex);
+    }
+}
