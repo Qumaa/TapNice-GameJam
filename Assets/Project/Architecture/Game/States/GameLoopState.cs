782422a06e60d1928fe706d5a3e37110f2eebc1f
Qma <mk348437@gmail.com>
2023-07-12 16:10:49 +0200

    implemented time displaying;
    todo - fix time output for level;

--- Assets/Project/Architecture/Game/States/GameLoopState.cs
+++ Assets/Project/Architecture/Game/States/GameLoopState.cs
@@ -1,18 +1,25 @@
-﻿namespace Project.Architecture
+﻿using Project.Game;
+using Project.UI;
+
+namespace Project.Architecture
 {
     public class GameLoopState : GameState
     {
         private readonly INextLevelResolver _levelResolver;
+        private readonly UIUpdater _uiUpdater;
 
         public GameLoopState(IGame game, IGameStateMachine stateMachine, INextLevelResolver levelResolver) :
             base(game, stateMachine)
         {
             _levelResolver = levelResolver;
+            _uiUpdater = new UIUpdater(_game.LoadedLevel);
         }
 
         public override void Enter()
         {
             _game.LoadedLevel.OnFinished += HandleLevelFinish;
+            _uiUpdater.Target = _game.UI.Get<IGameplayUI>();
+            _game.Add(_uiUpdater);
         }
 
         public override void Exit()
@@ -22,6 +29,7 @@
 
         private void HandleLevelFinish()
         {
+            _game.Remove(_uiUpdater);
             LoadNextLevelOrMainMenu();
         }
 
@@ -34,6 +42,20 @@
             }
 
             _stateMachine.SetState<KillGameState>();
+        }
+        
+        private class UIUpdater : IUpdatable
+        {
+            private readonly ILevel _source;
+            public IGameplayUI Target;
+
+            public UIUpdater(ILevel source)
+            {
+                _source = source;
+            }
+
+            public void Update(float timeStep) =>
+                Target.DisplayTime(_source.TimeElapsed);
         }
     }
 }
