114e30990298c95007310fe884e897795ee6f214
Qma <mk348437@gmail.com>
2023-07-12 12:47:10 +0200

    created game state sets;
    nested state machine that can be used inside different machines;

---
+++ Assets/Project/Architecture/Game/State machine/GamesStatesSet.cs
@@ -0,0 +1,16 @@
+﻿namespace Project.Architecture
+{
+    public abstract class GamesStatesSet : GameStateMachine, IGameState
+    {
+        public abstract void Exit();
+
+        public abstract void Enter();
+    }
+    
+    public abstract class GamesStatesSet<T> : GameStateMachine, IGameState<T>
+    {
+        public abstract void Exit();
+
+        public abstract void Enter(T arg);
+    }
+}
