91c2aa389b9d2f39b85aa83da8331767b662fd2e
Qma <mk348437@gmail.com>
2023-07-12 12:41:32 +0200

    reworked state machine;

--- Assets/Project/Architecture/Game/State machine/GameStateMachine.cs
+++ Assets/Project/Architecture/Game/State machine/GameStateMachine.cs
@@ -8,24 +8,14 @@ namespace Project.Architecture
         private readonly Dictionary<Type, IExitableGameState> _states = new();
         private IExitableGameState _currentState;
 
-        public IGameStateMachine AddState<T>(T state)
-            where T : IExitableGameState
-        {
+        public void AddSingleState<T>(T state) where T : IExitableGameState =>
             _states.Add(typeof(T), state);
-            return this;
-        }
 
-        public void SetState<T>()
-            where T : IEnterableGameState
-        {
+        public void SetState<T>() where T : IEnterableGameState =>
             SetStateInternal<T>().Enter();
-        }
 
-        public void SetState<T, TArg>(TArg arg)
-            where T : IEnterableGameState<TArg>
-        {
+        public void SetState<T, TArg>(TArg arg) where T : IEnterableGameState<TArg> =>
             SetStateInternal<T>().Enter(arg);
-        }
 
         private T SetStateInternal<T>()
         {
--- Assets/Project/Architecture/Game/State machine/IGameStateMachine.cs
+++ Assets/Project/Architecture/Game/State machine/IGameStateMachine.cs
@@ -2,13 +2,17 @@
 {
     public interface IGameStateMachine
     {
-        IGameStateMachine AddState<T>(T state)
-            where T : IExitableGameState;
+        void AddSingleState<T>(T state) where T : IExitableGameState;
+        void SetState<T>() where T : IEnterableGameState;
+        void SetState<T, TArg>(TArg arg) where T : IEnterableGameState<TArg>;
+    }
 
-        void SetState<T>()
-            where T : IEnterableGameState;
-
-        void SetState<T, TArg>(TArg arg)
-            where T : IEnterableGameState<TArg>;
+    public static class GameStateMachineExtensions
+    {
+        public static IGameStateMachine AddState<T>(this IGameStateMachine machine, T state) where T : IExitableGameState
+        {
+            machine.AddSingleState(state);
+            return machine;
+        }
     }
 }
